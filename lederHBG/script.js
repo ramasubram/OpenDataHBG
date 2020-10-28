dcb8dfc909c0f4a1cf4db0c0b9cef0ebeb978ad6
ramasubram <69894645+ramasubram@users.noreply.github.com>
2020-10-28 17:23:17 +0100

    Add files via upload

--- lederHBG/script.js
+++ lederHBG/script.js
@@ -1,7 +1,8 @@
-
 const app = document.getElementById('root');
 var lat, long, canvas;
 var request = new XMLHttpRequest();
+let map;
+// Script to access Helsingborg's open data set that contains information about the trails in Helsingborg
 request.open('GET', 'https://helsingborg.opendatasoft.com/api/records/1.0/search/?dataset=leder&q=&facet=lednamn', true);
 
 request.onload = function() {
@@ -9,6 +10,7 @@ request.onload = function() {
 
     if (request.status >= 200 && request.status < 400) {
         // create div element to contain the trail list
+        // Create div  for map
         const mainDiv = document.createElement('div');
         const mapDiv = document.createElement('div');
         mainDiv.setAttribute('class', 'main_container');
@@ -16,6 +18,7 @@ request.onload = function() {
         mapDiv.setAttribute('id', 'map_canvas');
         app.appendChild(mainDiv);
         app.appendChild(mapDiv);
+
         dataset.records.forEach(item => {
             // create button element for each trail
             const h3 = document.createElement('h3');
@@ -32,11 +35,10 @@ request.onload = function() {
                 lat = item.fields.geo_point_2d[0];
                 long =  item.fields.geo_point_2d[1];
 
-                showMapLoc();
-                drawShape(item.fields.geo_shape.coordinates);
+                // Call to function that positions map and mark the trail area
+                drawShape(item.fields.geo_shape.coordinates,item.fields.geo_shape.type);
             });
-            // end - define event for button
-            h3.appendChild(btn);        
+            h3.appendChild(btn);
         });
     }
     else{
@@ -46,8 +48,8 @@ request.onload = function() {
 
 request.send();
 
-let map;
-
+// Needs function initmap for the API script
+// it displays map that centers to the current location 
 function initMap() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
@@ -62,16 +64,7 @@ function initMap() {
     }
 }
 
-function showMapLoc() {
-    var myOptions = {
-        center: new google.maps.LatLng(lat, long),
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-}
-
+// Error handling for geolocation call
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(
@@ -81,26 +74,47 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     );
     infoWindow.open(map);
 }
-function drawShape(shapeCoords) {
-  const map = new google.maps.Map(document.getElementById("map_canvas"), {
-    zoom: 15,
-    center: new google.maps.LatLng(lat, long),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
 
-  var polygonCoords = [];
-  for (var i = 0; i < shapeCoords.length; i++) {
-      var pt = {lat: shapeCoords[i][1], lng: shapeCoords[i][0]};
-      polygonCoords.push(pt);
-    }
-  // Construct the polygon.
-  const shape = new google.maps.Polygon({
-    paths: polygonCoords,
-    strokeColor: "#FF0000",
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: "#FF0000",
-    fillOpacity: 0.35
+//shows the map centering to the coordinates lat and long
+function showMapLoc() {
+  map = new google.maps.Map(document.getElementById("map_canvas"), {
+      zoom: 15,
+      center: new google.maps.LatLng(lat, long),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
   });
-  shape.setMap(map);
+}
+
+// function to position the map and mark the area in the map
+// by drawing the polygin with the given coordinates
+function drawShape(shapeCoords,shapeType) {
+    showMapLoc();
+    var polygonCoords = [];
+    switch (shapeType) {
+    case "LineString":
+        for (var i = 0; i < shapeCoords.length; i++) {
+            var pt = {lat: shapeCoords[i][1], lng: shapeCoords[i][0]};
+            polygonCoords.push(pt);
+        }
+        break;
+    case "MultiLineString":
+        shapeCoords.forEach(arrayCoords => { 
+            for (var i = 0; i < arrayCoords.length; i++) {
+                var pt = {lat: arrayCoords[i][1], lng: arrayCoords[i][0]};
+                polygonCoords.push(pt);
+            }
+        });
+        break;
+    default:
+        break;
+    }
+    // Construct the polygon.
+    const shape = new google.maps.Polygon({
+        paths: polygonCoords,
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35
+    });
+    shape.setMap(map);
 }
